a3cc5a5e9649c9f9a7cc9bc1e301ff76febdff74
efsu707 <konnitiha0402@gmail.com>
2025-12-13 20:17:48 +0900

    sa

--- yamacorp.js
+++ yamacorp.js
@@ -23,3 +23,38 @@ window.onload = () => {
     alert("お問い合わせありがとうございます！");
   });
 };
+
+// Google Chartsライブラリをロード
+google.charts.load('current', { packages: ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  // 公開したGoogleスプレッドシートのURLを指定
+  // 【シートID】と【シート名】を実際のものに置き換えてください
+  var query = new google.visualization.Query(
+    'https://docs.google.com/spreadsheets/d/【シートID】/gviz/tq?sheet=【シート名】'
+  );
+
+  query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+  if (response.isError()) {
+    console.error('エラー: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+
+  var data = response.getDataTable();
+  var options = {
+    title: '最新データグラフ',
+    width: '100%',
+    height: 400,
+    legend: { position: 'bottom' },
+    hAxis: { title: '項目' },
+    vAxis: { title: '値' }
+  };
+
+  // 折れ線グラフを描画（必要に応じて BarChart や PieChart に変更可能）
+  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
